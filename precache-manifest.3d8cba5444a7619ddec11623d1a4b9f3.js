583faaa026a7ea1c0a10416328f557adbdc6b3fa
AnDJ <andj4cn@gmail.com>
2020-02-24 12:15:26 +0800

    feat: remove blog and deploy personal main page
    I'm sorry to announce that my blog has been transfered to gitee.
    The new blog website is 'https://an-dj.gitee.io/'
    
    Thanks for your focus.

---
+++ precache-manifest.3d8cba5444a7619ddec11623d1a4b9f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df43612037f6771787992ed4b311b21d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "419c5c0df0b2e5b858df",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e01078be808cdcb327c6",
+    "url": "./static/js/2.4136a6b7.chunk.js"
+  },
+  {
+    "revision": "419c5c0df0b2e5b858df",
+    "url": "./static/js/main.068ea794.chunk.js"
+  },
+  {
+    "revision": "9f4065ee2ce180b6faa0",
+    "url": "./static/js/runtime-main.b2b850e7.js"
+  },
+  {
+    "revision": "1b28d6e5fcdbd29e30e8fd020f61e6e5",
+    "url": "./static/media/andj.1b28d6e5.jpg"
+  }
+]);
